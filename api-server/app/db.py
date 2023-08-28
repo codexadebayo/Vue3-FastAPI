6b5c22f4ea0a7366130bca1e8beadbf516578c1f
badman978 <davidupwork01@gmail.com>
2023-08-28 10:35:38 +0100

    version 1.5

--- api-server/app/db.py
+++ api-server/app/db.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import User 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:d3spicabl3@localhost/diadem"
+setting=User()
+
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{setting.DATABASE_USERNAME}:{setting.DATABASE_PASSWORD}@{setting.DATABASE_HOSTNAME}/{setting.DATABASE_NAME}"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
--- api-server/app/models.py
+++ api-server/app/models.py
@@ -2,34 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text, Foreig
 from sqlalchemy.orm import relationship
 
 from .db import Base
-
-
-# class Categories(Base):
-#     __tablename__ ='categories'
-
-#     id = Column(Integer, primary_key=True, nullable=False)
-#     categories = relationship("categories", back_populates="products")
-
-
-
-# class Products(Base):
-#     __tablename__ = "products"
-
-#     id = Column(Integer, primary_key=True, nullable=False)
-#     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-#     strName = Column(String, nullable=False)
-#     strDescription = Column(String, nullable=False, unique=True)
-#     strImg = Column(String, nullable=False, unique=True)
-#     intPrice = Column(Integer, nullable= False)
-#     intInventory = Column(Integer)
-#     strCategory = Column(String, nullable=False)
-#     onSale = Column(Boolean, server_default='False')
-#     product_category = Column(Integer, ForeignKey("category.id"))
-
-#     owner = relationship("Categories", back_populates="products")
-
     
-
 class Category(Base):
     __tablename__ = "categories"
 
