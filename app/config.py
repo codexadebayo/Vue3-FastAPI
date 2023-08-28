6b25dc4055e6a090d7e63af182ab47f2f91a3507
Badman <davidupwork01@gmail.com>
2023-08-28 09:40:02 +0100

    aws-ready

--- app/config.py
+++ app/config.py
@@ -1,30 +1,10 @@
-from pydantic_settings import BaseSettings  # Import BaseSettings from pydantic_settings
-
-
-# class User(BaseSettings):
-#     strDATABASE_PASSWORD:str
-#     strDATABASE_USERNAME:str
-#     strDATABASE_NAME:str
-#     # strSECRET_KEY:str
-#     strDATABASE_HOSTNAME:str
-#    
-#     
-
-#     class Config:
-#         env_file =".env"
-
-
-
-
-# settings = User()
-
-
+from pydantic_settings import BaseSettings  
 
 class User(BaseSettings):
     DATABASE_PASSWORD: str
-    DATABASE_USERNAME: str = "postgres"
-    DATABASE_NAME: str = "diadem"
-    DATABASE_HOSTNAME: str = "localhost"
+    DATABASE_USERNAME: str
+    DATABASE_NAME: str
+    DATABASE_HOSTNAME: str
     # intACCESS_TOKEN_EXPIRE_MINUTES: int
     # strDATABASE_PORT: str
     # strALGORITHM: str
--- app/main.py
+++ app/main.py
@@ -3,25 +3,18 @@ from typing import List, Optional
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
-from pydantic_settings import BaseSettings
 from sqlalchemy import func
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends, Response, status, HTTPException
+from fastapi import FastAPI, Depends, status, HTTPException
 from .db import engine, get_db
 from . import models, response, schemas
 from .config import User
 
+setting = User() #login database with user details
 
-setting = User()
+app=FastAPI() #create API key
 
-
-
-
-
-
-app=FastAPI()
-
-models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine) #access tables in DB
 
 
 app.add_middleware(
@@ -33,7 +26,7 @@ app.add_middleware(
 
     # allow_origins=["http://localhost:3000"],
 )
-    
+#CORS to allow access from clients
 
 
 while True:
@@ -49,19 +42,7 @@ while True:
         print("Error :", error)
         time.sleep(2)
 
-
-
-
-# @app.get("/products", response_model=List[response.Items])
-# async def get_all_products (db: Session = Depends(get_db)):
-#     all_products = db.query(models.Product).all()
-#     return all_products
-
-# @app.get("/")
-# async def get_home_rand(db: Session = Depends(get_db)):
-#     query_home = db.query(models.Product).order_by(func.random())  # Use the random() function for random ordering
-#     random_products = query_home.limit(10).all()  # Limit the results to 10 random products
-#     return random_products
+        #DB handshake 
 
 
 @app.get("/", response_model=List[response.Items])
@@ -70,21 +51,12 @@ async def get_home_rand (db: Session=Depends(get_db)):
     random_products = query_home.limit(10).all()
     
     return random_products
-#I'm trying to add an sql query that allows me get random products from the database
-
-
-
 
 
 @app.get("/products", response_model=List[response.Items])
-async def get_all_products(db: Session = Depends(get_db), limit:Optional[int]= None, search:Optional[ str]="None"):
-    products_query = db.query(models.Product).filter(models.Product.strDescription.contains(search)).limit(limit) #can search for products by checking for keywords in the strDescription column. Append ?search="..." to path to use.
+async def get_all_products(db: Session = Depends(get_db), limit:Optional[int]= None, search:Optional[ str]="None"):#route parameters allows clients search, and limit response data.
+    products_query = db.query(models.Product).filter(models.Product.strDescription.contains(search)).limit(limit) 
     return products_query
-
-
-
-
-
 
 @app.get("/products/{strName}", response_model=List[response.Items])
 async def get_product( strName:str, db: Session = Depends(get_db)):
@@ -115,21 +87,6 @@ async def get_products_by_category(category: str, db: Session = Depends(get_db))
     return products
 
 
-
-
-# @app.get("/products/categories/{category}", response_model=response.Items)
-# async def get_all_product_by_category(category:str, db: Session = Depends(get_db)):
-#     product_query = db.query(models.Product).filter(models.Product.strCategory == category).all()
-
-#     if not product_query:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Page Not Found')
-    
-#     return product_query
-
-# @app.post("/products/categories")
-# async def add_product_category(db: Session = Depends(get_db)):
-
-
 @app.get("/products/searchid/{product_id}", response_model=response.Items)
 async def get_product_by_name(product_id:int, db: Session = Depends(get_db)):
     product_query = db.query(models.Product).filter(models.Product.id == product_id).first()
@@ -138,4 +95,3 @@ async def get_product_by_name(product_id:int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Page Not Found')
     
     return product_query
-    
